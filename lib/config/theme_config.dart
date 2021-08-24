39acf6262ea9da00e7b727b2a451853f5b6ea673
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-24 21:10:18 +0530

    Designed the appbar

--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -4,6 +4,27 @@ final lightTheme = ThemeData.light().copyWith(
   scaffoldBackgroundColor: Colors.white,
   primaryColor: Colors.black,
   accentColor: Colors.lightBlue,
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(
+        EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 16.0,
+        ),
+      ),
+      backgroundColor: MaterialStateProperty.all(Colors.white),
+      foregroundColor: MaterialStateProperty.all(Colors.black),
+      overlayColor: MaterialStateProperty.all(Colors.white),
+    ),
+  ),
+  iconTheme: IconThemeData(
+    color: Colors.black,
+  ),
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    actionsIconTheme: IconThemeData(color: Colors.black),
+    foregroundColor: Colors.black,
+  ),
 );
 
 final darkTheme = ThemeData.dark().copyWith(
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -8,11 +8,19 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isMobile = SizerUtil.width <= mobileWidth;
-
     return SliverAppBar(
       centerTitle: isMobile ? true : false,
-      title: Text('ankan.dev'),
-      leading: isMobile ? Icon(Icons.menu) : null,
+      title: Image.asset(
+        'assets/images/logo_light.png',
+        height: isMobile ? 10.h : 5.h,
+        fit: BoxFit.fitHeight,
+      ),
+      leading: isMobile
+          ? Icon(
+              Icons.menu,
+              color: Theme.of(context).iconTheme.color,
+            )
+          : null,
       actions: isMobile
           ? [
               IconButton(
@@ -25,18 +33,22 @@ class CustomAppBar extends StatelessWidget {
                 onPressed: () {},
                 child: Text('About'),
               ),
+              SizedBox(width: 16.0),
               TextButton(
                 onPressed: () {},
                 child: Text('Projects'),
               ),
+              SizedBox(width: 16.0),
               TextButton(
                 onPressed: () {},
                 child: Text('Education'),
               ),
+              SizedBox(width: 16.0),
               TextButton(
                 onPressed: () {},
                 child: Text('Contact'),
               ),
+              SizedBox(width: 16.0),
               IconButton(
                 onPressed: () {},
                 icon: Icon(Icons.dark_mode),
