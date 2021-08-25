39fa3afed5a384737d0f028e7108871c38e72bd3
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-25 19:45:11 +0530

    Finished adding all text

--- lib/sections/featured_section.dart
+++ lib/sections/featured_section.dart
@@ -11,21 +11,82 @@ class FeaturedSection extends StatelessWidget {
     final isMobile = SizerUtil.width <= mobileWidth;
     return Container(
       margin: EdgeInsets.only(top: isMobile ? 3.h : 2.h),
-      width: 100.h,
+      width: isMobile ? 90.w : 150.w,
       child: Column(
         children: [
           Header(text: 'Featured Project'),
           SizedBox(
-            height: 1.w,
+            height: 5.w,
           ),
           ClipRRect(
             borderRadius: BorderRadius.circular(isMobile ? 15.0 : 25.0),
             child: Image.asset(
               'assets/images/featured_project.png',
-              width: isMobile ? 80.w : 150.w,
+              width: isMobile ? 90.w : 120.w,
               fit: BoxFit.fitWidth,
             ),
           ),
+          SizedBox(height: 3.w),
+          Container(
+            width: isMobile ? 90.w : 150.w,
+            child: Text(
+              'Wuphf Chat is a full fledged chatting application made with Flutter.',
+              style: TextStyle(
+                fontSize: isMobile ? 14.sp : 10.sp,
+              ),
+              textAlign: TextAlign.start,
+              // textAlign: TextAlign.,
+            ),
+          ),
+          SizedBox(height: 2.w),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 1.w),
+            child: UnorderedList([
+              'Open Source',
+              '1 to 1 Chat',
+              'Group Chat',
+              'Get Notifications for New Chat and New Groups',
+              'Robust User Presence',
+              'Send Emojis and Images',
+              'No need to refresh. Everything is updated realtime.',
+            ]),
+          ),
+          SizedBox(height: 2.5.w),
+          Container(
+            width: isMobile ? 90.w : 150.w,
+            child: Text(
+              'Built using:',
+              style: TextStyle(
+                fontSize: isMobile ? 14.sp : 10.sp,
+              ),
+              textAlign: TextAlign.start,
+              // textAlign: TextAlign.,
+            ),
+          ),
+          SizedBox(height: 2.w),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 1.w),
+            child: UnorderedList([
+              'Main Database: Firebase Firestore',
+              'User Presence Database: Firebase Realtime Database',
+              'Notifications: Firebase Cloud Messaging & Cloud Functions',
+              'State Management: BLOC',
+              'Observing Streams: RxDart',
+            ]),
+          ),
+          SizedBox(height: 2.w),
+          Container(
+            width: isMobile ? 90.w : 150.w,
+            child: Text(
+              'For more information click the GitHub or Play Store links below.',
+              style: TextStyle(
+                fontSize: isMobile ? 14.sp : 10.sp,
+              ),
+              textAlign: TextAlign.start,
+              // textAlign: TextAlign.,
+            ),
+          ),
+          SizedBox(height: 1.w),
         ],
       ),
     );
