a5c1e05b5d042eb858494a17505ce2b8e2cd9603
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-26 15:52:54 +0530

    Created link button for mobile

--- lib/sections/contact_section.dart
+++ lib/sections/contact_section.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sizer/sizer.dart';
+
 import 'package:ankan_site/config/configs.dart';
 import 'package:ankan_site/widgets/widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
 
 class ContactSection extends StatelessWidget {
   const ContactSection({Key? key}) : super(key: key);
@@ -18,7 +20,124 @@ class ContactSection extends StatelessWidget {
           SizedBox(
             height: 5.w,
           ),
+          Text(
+            'Feel free to contact me if you have any queries or issues',
+            style: TextStyle(fontSize: isMobile ? 14.sp : 10.sp),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 2.w),
+          isMobile
+              ? Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        LinkButton(
+                          icon: FontAwesomeIcons.linkedin,
+                          color: Color(0XFF0077b5),
+                          title: 'LinkedIn',
+                          fontSize: 14.sp,
+                          onTap: () {},
+                        ),
+                        SizedBox(width: 4.w),
+                        LinkButton(
+                          icon: FontAwesomeIcons.github,
+                          color: Color(0xff333333),
+                          title: 'GitHub',
+                          fontSize: 14.sp,
+                          onTap: () {},
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 2.w),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        LinkButton(
+                          icon: FontAwesomeIcons.instagram,
+                          color: Color(0XFF833ab4),
+                          title: 'Instagram',
+                          fontSize: 14.sp,
+                          onTap: () {},
+                        ),
+                        SizedBox(width: 4.w),
+                        LinkButton(
+                          icon: FontAwesomeIcons.twitter,
+                          color: Color(0xff1da1f2),
+                          title: 'Twitter',
+                          fontSize: 14.sp,
+                          onTap: () {},
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 2.w),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        LinkButton(
+                          icon: FontAwesomeIcons.facebook,
+                          color: Color(0XFF4267B2),
+                          title: 'Facebook',
+                          fontSize: 14.sp,
+                          onTap: () {},
+                        ),
+                        SizedBox(width: 4.w),
+                        LinkButton(
+                          icon: FontAwesomeIcons.solidEnvelope,
+                          color: Color(0xffdd4b39),
+                          title: 'Email',
+                          fontSize: 14.sp,
+                          onTap: () {},
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              : Row(),
         ],
+      ),
+    );
+  }
+}
+
+class LinkButton extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String title;
+  final double fontSize;
+  final void Function() onTap;
+
+  const LinkButton({
+    Key? key,
+    required this.icon,
+    required this.color,
+    required this.title,
+    required this.fontSize,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton.icon(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(
+            EdgeInsets.all(4.w),
+          ),
+          backgroundColor: MaterialStateProperty.all(color),
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+        onPressed: onTap,
+        icon: FaIcon(
+          icon,
+          size: 20.sp,
+        ),
+        label: Text(
+          title,
+          style: TextStyle(
+            fontSize: fontSize,
+          ),
+        ),
       ),
     );
   }
