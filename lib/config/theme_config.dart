e04584138cc515cd7bbd9e3f24677970c46ba84a
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-27 16:23:00 +0530

    Defined dark theme colors

--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -39,8 +39,42 @@ final lightTheme = ThemeData(
   ),
 );
 
-final darkTheme = ThemeData.dark().copyWith(
-  scaffoldBackgroundColor: Colors.grey.shade800,
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  fontFamily: 'OpenSans',
+  scaffoldBackgroundColor: Colors.black,
   primaryColor: Colors.white,
   accentColor: Colors.lightBlue,
+  hintColor: Colors.grey.shade900,
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(
+        EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 16.0,
+        ),
+      ),
+      backgroundColor: MaterialStateProperty.all(Colors.white),
+      foregroundColor: MaterialStateProperty.all(Colors.black),
+      overlayColor: MaterialStateProperty.all(Colors.white),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      ),
+    ),
+  ),
+  iconTheme: IconThemeData(
+    color: Colors.white,
+  ),
+  appBarTheme: AppBarTheme(
+    color: Colors.grey.shade900,
+    actionsIconTheme: IconThemeData(color: Colors.white),
+    foregroundColor: Colors.white,
+    shadowColor: Colors.grey.shade800,
+  ),
 );
