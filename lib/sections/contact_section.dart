f3e7322848d212cf4331948a13ba18de248f0f4d
Ankan Sikdar <ankansikdar@gmail.com>
2021-11-14 22:03:01 +0530

    Moved link button class to seperate file

--- lib/sections/contact_section.dart
+++ lib/sections/contact_section.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sizer/sizer.dart';
 import 'dart:js' as js;
-
 import 'package:ankan_site/config/configs.dart';
 import 'package:ankan_site/widgets/widgets.dart';
 
@@ -220,85 +219,6 @@ class ContactSection extends StatelessWidget {
                   ],
                 ),
         ],
-      ),
-    );
-  }
-}
-
-class LinkButton extends StatelessWidget {
-  final IconData icon;
-  final Color color;
-  final String title;
-  final void Function() onTap;
-
-  const LinkButton({
-    Key? key,
-    required this.icon,
-    required this.color,
-    required this.title,
-    required this.onTap,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ElevatedButton.icon(
-        style: ButtonStyle(
-          padding: MaterialStateProperty.all(
-            EdgeInsets.all(4.w),
-          ),
-          backgroundColor: MaterialStateProperty.all(color),
-        ),
-        onPressed: onTap,
-        icon: FaIcon(
-          icon,
-          size: 20.sp,
-        ),
-        label: Text(
-          title,
-          style: TextStyle(
-            fontSize: 14.sp,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class PCLinkButton extends StatelessWidget {
-  final IconData icon;
-  final Color color;
-  final String title;
-  final void Function() onTap;
-
-  const PCLinkButton({
-    Key? key,
-    required this.icon,
-    required this.color,
-    required this.title,
-    required this.onTap,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      style: ButtonStyle(
-        padding: MaterialStateProperty.all(
-          EdgeInsets.symmetric(horizontal: 4.w, vertical: 3.w),
-        ),
-        minimumSize: MaterialStateProperty.all(Size(35.w, 2.w)),
-        backgroundColor: MaterialStateProperty.all(color),
-      ),
-      onPressed: onTap,
-      icon: FaIcon(
-        icon,
-        size: 15.sp,
-      ),
-      label: Text(
-        title,
-        style: TextStyle(
-          fontSize: 8.sp,
-        ),
       ),
     );
   }
