a837765657afd312d7da0291d9f4955d62f6ff1c
Ankan Sikdar <ankansikdar@gmail.com>
2021-11-15 20:14:48 +0530

    Used wrap to make contact links more responsive

--- lib/sections/contact_section.dart
+++ lib/sections/contact_section.dart
@@ -34,190 +34,88 @@ class ContactSection extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 3.w),
-          isMobile
-              ? Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        LinkButton(
-                          icon: FontAwesomeIcons.linkedin,
-                          color: Color(0XFF0077b5),
-                          title: 'LinkedIn',
-                          onTap: () {
-                            handleTap(
-                                'https://www.linkedin.com/in/ankansikdar/');
-                          },
-                        ),
-                        SizedBox(width: 4.w),
-                        LinkButton(
-                          icon: FontAwesomeIcons.github,
-                          color: Color(0xff333333),
-                          title: 'GitHub',
-                          onTap: () {
-                            handleTap('https://github.com/ankanSikdar');
-                          },
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 2.w),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        LinkButton(
-                          icon: FontAwesomeIcons.instagram,
-                          color: Color(0XFF833ab4),
-                          title: 'Instagram',
-                          onTap: () {
-                            handleTap(
-                                'https://www.instagram.com/ankan_sikdar/');
-                          },
-                        ),
-                        SizedBox(width: 4.w),
-                        LinkButton(
-                          icon: FontAwesomeIcons.twitter,
-                          color: Color(0xff1da1f2),
-                          title: 'Twitter',
-                          onTap: () {
-                            handleTap('https://twitter.com/ankan_sikdar');
-                          },
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 2.w),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        LinkButton(
-                          icon: FontAwesomeIcons.facebook,
-                          color: Color(0XFF4267B2),
-                          title: 'Facebook',
-                          onTap: () {
-                            handleTap('https://www.facebook.com/ankanSikdar/');
-                          },
-                        ),
-                        SizedBox(width: 4.w),
-                        LinkButton(
-                          icon: FontAwesomeIcons.solidEnvelope,
-                          color: Color(0xffdd4b39),
-                          title: 'Email',
-                          onTap: () {
-                            handleTap('mailto:contact@ankan.dev');
-                          },
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 2.w),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        LinkButton(
-                          icon: FontAwesomeIcons.reddit,
-                          color: Color(0xffff4500),
-                          title: 'Reddit',
-                          onTap: () {
-                            handleTap('https://www.reddit.com/user/Chrome597');
-                          },
-                        ),
-                        SizedBox(width: 4.w),
-                        LinkButton(
-                          icon: FontAwesomeIcons.googlePlay,
-                          color: Color(0xff78C257),
-                          title: 'Play Store',
-                          onTap: () {
-                            handleTap(
-                                'https://play.google.com/store/apps/dev?id=6059531150910740578');
-                          },
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              : Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PCLinkButton(
-                            icon: FontAwesomeIcons.linkedin,
-                            color: Color(0XFF0077b5),
-                            title: 'LinkedIn',
-                            onTap: () {
-                              handleTap(
-                                  'https://www.linkedin.com/in/ankansikdar/');
-                            }),
-                        SizedBox(width: 2.w),
-                        PCLinkButton(
-                            icon: FontAwesomeIcons.github,
-                            color: Color(0xff333333),
-                            title: 'GitHub',
-                            onTap: () {
-                              handleTap('https://github.com/ankanSikdar');
-                            }),
-                        SizedBox(width: 2.w),
-                        PCLinkButton(
-                          icon: FontAwesomeIcons.twitter,
-                          color: Color(0xff1da1f2),
-                          title: 'Twitter',
-                          onTap: () {
-                            handleTap('https://twitter.com/ankan_sikdar');
-                          },
-                        ),
-                        SizedBox(width: 2.w),
-                        PCLinkButton(
-                          icon: FontAwesomeIcons.instagram,
-                          color: Color(0XFF833ab4),
-                          title: 'Instagram',
-                          onTap: () {
-                            handleTap(
-                                'https://www.instagram.com/ankan_sikdar/');
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 2.w),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PCLinkButton(
-                            icon: FontAwesomeIcons.facebook,
-                            color: Color(0XFF4267B2),
-                            title: 'Facebook',
-                            onTap: () {
-                              handleTap(
-                                  'https://www.facebook.com/ankanSikdar/');
-                            }),
-                        SizedBox(width: 2.w),
-                        PCLinkButton(
-                            icon: FontAwesomeIcons.solidEnvelope,
-                            color: Color(0xffdd4b39),
-                            title: 'Email',
-                            onTap: () {
-                              handleTap('mailto:contact@ankan.dev');
-                            }),
-                        SizedBox(width: 2.w),
-                        PCLinkButton(
-                          icon: FontAwesomeIcons.reddit,
-                          color: Color(0xffff4500),
-                          title: 'Reddit',
-                          onTap: () {
-                            handleTap('https://www.reddit.com/user/Chrome597');
-                          },
-                        ),
-                        SizedBox(width: 2.w),
-                        PCLinkButton(
-                          icon: FontAwesomeIcons.googlePlay,
-                          color: Color(0xff78C257),
-                          title: 'Play Store',
-                          onTap: () {
-                            handleTap(
-                                'https://play.google.com/store/apps/dev?id=6059531150910740578');
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+          Wrap(
+            alignment: WrapAlignment.spaceBetween,
+            direction: Axis.horizontal,
+            runSpacing: 5.w,
+            spacing: isMobile ? 5.w : 10.w,
+            children: [
+              LinkButton(
+                icon: FontAwesomeIcons.linkedin,
+                color: Color(0XFF0077b5),
+                title: 'LinkedIn',
+                onTap: () {
+                  handleTap('https://www.linkedin.com/in/ankansikdar/');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.github,
+                color: Color(0xff333333),
+                title: 'GitHub',
+                onTap: () {
+                  handleTap('https://github.com/ankanSikdar');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.instagram,
+                color: Color(0XFF833ab4),
+                title: 'Instagram',
+                onTap: () {
+                  handleTap('https://www.instagram.com/ankan_sikdar/');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.twitter,
+                color: Color(0xff1da1f2),
+                title: 'Twitter',
+                onTap: () {
+                  handleTap('https://twitter.com/ankan_sikdar');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.facebook,
+                color: Color(0XFF4267B2),
+                title: 'Facebook',
+                onTap: () {
+                  handleTap('https://www.facebook.com/ankanSikdar/');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.solidEnvelope,
+                color: Color(0xffdd4b39),
+                title: 'Email',
+                onTap: () {
+                  handleTap('mailto:contact@ankan.dev');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.reddit,
+                color: Color(0xffff4500),
+                title: 'Reddit',
+                onTap: () {
+                  handleTap('https://www.reddit.com/user/Chrome597');
+                },
+                isMobile: isMobile,
+              ),
+              LinkButton(
+                icon: FontAwesomeIcons.googlePlay,
+                color: Color(0xff78C257),
+                title: 'Play Store',
+                onTap: () {
+                  handleTap(
+                      'https://play.google.com/store/apps/dev?id=6059531150910740578');
+                },
+                isMobile: isMobile,
+              )
+            ],
+          ),
+          SizedBox(height: 2.w),
         ],
       ),
     );
